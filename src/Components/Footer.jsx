fa1676652b3591b5a1ad917919561e52ae85ce9f
Kashish Jethwani <kashishjethwani7990@gmail.com>
2026-01-19 11:07:23 +0530

    add margin to footer for spacing

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="w-full py-4 text-slate-400 text-center text-sm  mt-20">
+    <footer className="w-full py-4 text-slate-400 text-center text-sm  mt-20 mb-10">
       © 2026 Simple PWA Demo
     </footer>
   )
